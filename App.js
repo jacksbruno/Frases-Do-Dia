553c8fe2f8d80d645cfdbdd6c0fd8425049826ef
Jacks Bruno <jacksbruno@MacBook-Air-de-Jacks.local>
2019-03-17 13:39:19 -0400

    Começando a implementanção do app Frases do Dia

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+const gerarFrase = () => {
+  alert('Gerar Frase Randômica')
+};
 
 export default class App extends Component {
-  render() {
+  render() {  
     return (
-      <Text style={estilos.texto}>Olá mundo criado por Deus!</Text>
+      <View style={estilos.Principal}>
+        <Image source={require('./imgs/logo.png')} />
+        <TouchableOpacity onPress={gerarFrase} style={estilos.botao}>
+          <Text style={estilos.texto}>Ver Frase</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const estilos = StyleSheet.create({
+  Principal: {
+    paddingTop: 40,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  botao: {
+    padding: 10,
+    backgroundColor: '#538530',
+    marginTop: 15
+  },
   texto: {
-    marginTop: 30,
-    fontSize: 25
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 });
